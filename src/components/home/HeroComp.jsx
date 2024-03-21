f0b491c2b88040d22877bbc1a1f16542e9fd2d40
iysaofficial <youngscientist.iysa@gmail.com>
2024-03-21 14:45:53 +0700

    revisi hero

--- src/components/home/HeroComp.jsx
+++ src/components/home/HeroComp.jsx
@@ -5,7 +5,7 @@ const HeroComp = () =>{
             <div className="hero-container">
                 <img src="./assets/images/logo/NYCO.png" alt=""/>
                 <h1>National Youth Chemistry Olympiad</h1>
-                <p>Pendaftaran Olimpiade Kimia Remaja Internasional telah dibuka! Bergabunglah <br /> dengan acara bergengsi ini dan dapatkan pengalaman kimia yang tak terlupakan!
+                <p>Pendaftaran National Youth Chemistry Olympiad telah dibuka! Bergabunglah <br /> dengan acara bergengsi ini dan dapatkan pengalaman yang tak terlupakan!
                 </p>
                 <br />
                 <a href="https://registrasi-nyco.vercel.app/" target="_blank" className="registration-button">Daftar</a>
