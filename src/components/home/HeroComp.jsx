6bedb82cf3052c6f22b59d2bb32ce2b57ba90252
iysaofficial <youngscientist.iysa@gmail.com>
2025-05-19 08:45:40 +0700

    close

--- src/components/home/HeroComp.jsx
+++ src/components/home/HeroComp.jsx
@@ -8,7 +8,7 @@ const HeroComp = () =>{
                 {/* <p>Pendaftaran National Youth Chemistry Olympiad telah dibuka! Bergabunglah <br /> dengan acara bergengsi ini dan dapatkan pengalaman yang tak terlupakan!
                 </p> */}
                  <br />
-                <a href="#" target="_blank" className="registration-button">Registrasi</a>
+                <a href="#" target="_blank" className="registration-button">Registrasi di tutup</a>
             </div>
         </section>
         </> 
